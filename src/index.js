a3d37dc9577c2e6f251c2265d38068d3c5a382d2
Rana Kelani <rana.1926@hotmail.com>
2017-06-19 14:02:52 +0300

    Basic sign up page with firebase auth

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
+firebaseApp.auth().onAuthStateChanged(user => {
+	if(user){
+		console.log('user has signed in or up', user)
+	} else{
+		console.log('user has signed out or still needs to sign in')
+	}
+})
+
 ReactDOM.render(
 	<Router path="/" history={browserHistory}>
-		<Router path="/app" component={App}/>
-		<Router path="/signin" component={SignIn}/>
-		<Router path="/signup" component={SignUp}/>
+		<Route path="/app" component={App}/>
+		<Route path="/signin" component={SignIn}/>
+		<Route path="/signup" component={SignUp}/>
 	</Router>
 	, document.getElementById('root')
 )
